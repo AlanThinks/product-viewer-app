c426e2ee43abf004c7e9bda7aba169d172a6d525
AlanThinks <a@alanthinks.com>
2018-10-09 22:09:38 -0400

    404 Page

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -4,10 +4,10 @@ import React, { Component } from "react"
 export default class NotFound extends Component {
   render() {
     return (
-      <div>
+      <div style={{ marginTop: 100, marginLeft: 50 }}>
         <h1>Oops!</h1>
         <p>404: Sorry! Can't Find That Page</p>
-        <Link to="./">Click Here To Go Home</Link>
+        <Link to="/">Click Here To Go Home</Link>
       </div>
     )
   }
